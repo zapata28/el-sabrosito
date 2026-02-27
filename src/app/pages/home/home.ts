b0298361e2c531d65d4520c47dd6a6e2206df76d
santiago zapata <santiagozapatator28@gmail.com>
2026-02-27 17:42:47 -0500

    mucho contenido nuevo

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css']
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
 
+  imagenes: string[] = [
+    '/carrousel/11.png',
+    '/carrousel/12.png',
+    '/carrousel/13.png',
+  ];
+
+  indiceActual = 0;
+  intervalo: any;
+
+  ngOnInit() {
+    this.iniciarAutoplay();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalo);
+  }
+
+  siguiente() {
+    this.indiceActual = (this.indiceActual + 1) % this.imagenes.length;
+  }
+
+  anterior() {
+    this.indiceActual =
+      (this.indiceActual - 1 + this.imagenes.length) % this.imagenes.length;
+  }
+
+  irA(indice: number) {
+    this.indiceActual = indice;
+  }
+
+  iniciarAutoplay() {
+    this.intervalo = setInterval(() => {
+      this.siguiente();
+    }, 4000); // cambia cada 4 segundos
+  }
 }
